8cdf9f6316729de474bff02feea9e2d107f1ea31
Luis-Trigueiro <marquestrigueiro@gmail.com>
2022-03-10 18:12:29 +0000

    add soud

--- script.js
+++ script.js
@@ -40,16 +40,16 @@ var question = () => {
     var questionUser = document.getElementById("question").value;
     $("#questionUser").html(questionUser);
     if (questionUser.match(/name/)) {
-        $("#bot").html('<p class="robotAnswer"><span style="font-size: 14px;">🤖</span></br>My name is Dudu Bot and my master is Luis Eduardo Marques</p>');
+        $("#bot").html('<p class="robotAnswer"><audio autoplay> <source src="sound/robot.mp3"type="audio/mpeg"></audio><span style="font-size: 14px;">🤖</span></br>My name is Dudu Bot and my master is Luis Eduardo Marques</p>');
     } else if (questionUser.match(/(nationality|country|nation)/)) {
-        $("#bot").html('<p class="robotAnswer"><span style="font-size: 14px;">🤖</span></br>My master has Portuguese and Brazilian citizenship</p>');
+        $("#bot").html('<p class="robotAnswer"><audio autoplay> <source src="sound/robot.mp3"type="audio/mpeg"></audio><span style="font-size: 14px;">🤖</span></br>My master has Portuguese and Brazilian citizenship</p>');
 
     } else if (questionUser.match(/(mail|contact)/)) {
-        $("#bot").html('<p class="robotAnswer"><span style="font-size: 14px;">🤖</span></br>Contact email is marquestrigueiro@gmail.com</p>');
+        $("#bot").html('<p class="robotAnswer"><audio autoplay> <source src="sound/robot.mp3"type="audio/mpeg"></audio><span style="font-size: 14px;">🤖</span></br>Contact email is marquestrigueiro@gmail.com</p>');
     } else if (questionUser.match(/(language|stack)/)) {
-        $("#bot").html('<p class="robotAnswer"><span style="font-size: 14px;">🤖</span></br>Do you want to dowload my CV to have this information? <a target="_blank" href="doc/Luis Eduardo Trigueiro CV PT_F.pdf">click</a></p>');
+        $("#bot").html('<p class="robotAnswer"><audio autoplay> <source src="sound/robot.mp3"type="audio/mpeg"></audio><span style="font-size: 14px;">🤖</span></br>Do you want to dowload my CV to have this information? <a target="_blank" href="doc/Luis Eduardo Trigueiro CV PT_F.pdf">click</a></p>');
     }else{
-        $("#bot").html('<p class="robotAnswer"><span style="font-size: 14px;">🤖</span></br>I do not know this information. Maybe here. <a target="_blank" href="doc/Luis Eduardo Trigueiro CV PT_F.pdf">click</a></p>');
+        $("#bot").html('<p class="robotAnswer"><audio autoplay> <source src="sound/robot.mp3"type="audio/mpeg"></audio><span style="font-size: 14px;">🤖</span></br>I do not know this information. Maybe here. <a target="_blank" href="doc/Luis Eduardo Trigueiro CV PT_F.pdf">click</a></p>');
 
     }
 
